1d0c4ef59204520b699abd3a187afd0c8ec74293
eqdang <elizabethqdang@gmail.com>
2020-03-22 17:21:57 -0700

    tabulation project complete

--- W19D3/tabulation_project/lib/leet_code_70.js
+++ W19D3/tabulation_project/lib/leet_code_70.js
@@ -1,6 +1,16 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
+// You are climbing a stair case. It takes n steps to reach to the top. Each time you either climb 1 or 2 steps. In how many disticnt ways can you climb to the top? 
+
 
 function climbStairs(n) {
+	let table = new Array(n+1);
+	table[0] = 1;
+	table[1] = 1;
 
+	for (let i=2; i < table.length; i++) {
+		table[i] = table[i-1] + table[i-2];
+	}
+
+	return table[table.length-1]
 }
--- W19D3/tabulation_project/lib/problems.js
+++ W19D3/tabulation_project/lib/problems.js
@@ -21,9 +21,36 @@
 // stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
 // stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
 function stepper(nums) {
+	let table = new Array(nums.length).fill(false);
+	table[0] = true;
 
+	for (let i = 0; i < table.length; i++) {
+			if (table[i] === true) {
+				let maxRange = nums[i];
+				for (let j = 1; j <= maxRange; j++) {
+					table[j] = true;
+				}
+			}
+	}
+
+	return table[table.length - 1];
 }
 
+function stepperMemo(nums, memo={}) {
+	if (nums.length in memo) return memo[nums.length];
+	if (nums.length === 0) return true;
+
+	let maxRange = nums[0];
+	for (let step = 1; step <= maxRange; step++) {
+		if (stepperMemo(nums.slice(step), memo)) {
+			memo[nums.length] = true;
+			return true;
+		}
+	}
+		
+	memo[nums.length] = false;
+	return false;
+}
 
 // Write a function, maxNonAdjacentSum(nums), that takes in an array of nonnegative numbers.
 // The function should return the maximum sum of elements in the array we can get if we cannot take
@@ -36,7 +63,32 @@ function stepper(nums) {
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
 function maxNonAdjacentSum(nums) {
+	if (nums.length === 0) return 0;
+	let table = new Array(nums.length).fill(0);
+	table[0] = nums[0];
 
+	for (let i = 1; i < table.length; i++) {
+		let skipLeft = table[i-2] === undefined ? 0 : table[i-2];
+		let includedNum = skipLeft + nums[i];
+		let notIncludedNum = table[i-1];
+		table[i] = Math.max(includedNum, notIncludedNum);
+	}
+
+	return table[table.length -1];
+}
+
+function maxNonAdjacentSumMemo(nums, memo={}) {
+	if (nums.length in memo) return memo[nums.length];
+	if (nums.length === 0) return 0;
+
+	let firstEle = nums[0];
+
+	memo[nums.length] = Math.max(
+		firstEle + maxNonAdjacentSumMemo(nums.slice(2), memo),
+		maxNonAdjacentSumMemo(nums.slice(1), memo)
+	);
+
+	return memo[nums.length];
 }
 
 
@@ -53,7 +105,20 @@ function maxNonAdjacentSum(nums) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount) {
+	let table = new Array(amount + 1).fill(Infinity);
+	table[0] = 0;
 
+	coins.forEach(val => {
+		for (let amt=0; amt < table.length; amt++) {
+			for (let qty=0; qty * val <= amt; qty++) {
+				remainder = amt - (qty * val);
+				let attempt = table[remainder] + qty;
+				if (attempt < table[amt]) table[amt] = attempt;
+			}
+		}
+	});
+
+	return table[table.length-1];
 }
 
 
