50a3aa2c5b7a137070b3e44045c7133632ac4812
elizabethqdang <elizabethqdang@gmail.com>
2021-03-09 13:53:12 -0800

    quick sort project

--- W19D5/quick_sort_project/lib/quick_sort.js
+++ W19D5/quick_sort_project/lib/quick_sort.js
@@ -1,7 +1,39 @@
 function quickSort(array) {
+if (array.length <= 1) return array;
 
+let pivot = array.shift();
+let left = array.filter(el => el < pivot);
+let right = array.filter(el => el >= pivot);
+
+let leftSorted = quickSort(left);
+let rightSorted = quickSort(right);
+
+// return leftSorted.concat(pivot).concat(rightSorted);
+return [...leftSorted, pivot, ...rightSorted ];
 }
 
+function partition(array, pivot) {
+if (array.length <= 1) return array;
+
+	let left = [];
+	let right = [];
+
+array.forEach(el => {
+	if (el < pivot) {
+		left.push(el);
+	} else {
+		right.push(el);
+	}
+});
+	return [left, right];
+}
+
+
+function partitian(array, pivot) {
+	let left = array.filter(el => el < pivot);
+	let right = array.filter(el => el >= pivot);
+	return [left, right];
+}
 
 module.exports = {
     quickSort
